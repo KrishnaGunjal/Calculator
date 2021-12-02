200a129b34742b24d4bc9c270515bdae87510853
Krishna Gunjal <kgunjal51@gmail.com>
2021-12-02 21:58:21 +0530

    Added Global env.

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -7,14 +7,70 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    let buttons = [
-        ["7", "8", "9", "X"],
-        ["4", "5", "6", "-"],
-        ["1", "2", "3", "+"],
-        ["0", ".", ".", "="]
-    ]
+class GlobalEnvironment: ObservableObject {
+    @Published var display = "00"
     
+    func receiveData(calculatorButton: calcButton) {
+        self.display = calculatorButton.title
+    }
+}
+
+enum CalcButtons: String {
+    case zero, one, two, three, four, five, six, seven, eight, nine
+    case equals, plus, minus, multiply, divide
+    case decimal
+    case ac, plusminus, percent
+    
+    var title: String {
+        switch self {
+        case .zero: return "0"
+        case .one: return "1"
+        case .two: return "2"
+        case .three: return "3"
+        case .four: return "4"
+        case .five: return "5"
+        case .six: return "6"
+        case .seven: return "7"
+        case .eight: return "8"
+        case .nine: return "9"
+        case .plus: return "+"
+        case .minus: return "-"
+        case .multiply: return "X"
+        case .plusminus: return "+/-"
+        case .percent: return "%"
+        case .equals: return "="
+        case .decimal: return "."
+        default:
+            return "AC"
+        }
+    }
+    
+    
+    var backgroundColor: Color {
+        switch self {
+        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine:
+            return Color(.darkGray)
+        case .ac, .plusminus, .percent:
+            return Color(.lightGray)
+        default:
+            return Color(.orange)
+        }
+    }
+    
+}
+
+struct ContentView: View {
+    @EnvironmentObject var env: GlobalEnvironment
+    
+    
+    
+    let buttons: [[CalcButtons]] = [
+        [.ac, .plusminus, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .plus],
+        [.zero, .decimal, .equals]
+    ]
     
     var body: some View {
         
@@ -24,28 +80,36 @@ struct ContentView: View {
                 //Textfield
                 HStack{
                     Spacer()
-                    Text("42")
+                    Text(env.display)
                         .foregroundColor(.white)
                         .font(.system(size: 64))
                     
                 }.padding()
+                
                 //buttons
                 ForEach(buttons, id:\.self){row in
                     HStack(spacing: 12){
                         ForEach(row, id:\.self){button in
-                            Text(button)
+                            Button(action: {
+                                self.env.receiveData(calculatorButton: button)
+                                
+                            }){Text(button.title)
                                 .font(.system(size: 32))
-                                .frame(width: self.buttonWidth(), height: self.buttonWidth())
+                                .frame(width: self.buttonWidth(button: button), height: (UIScreen.main.bounds.width - 5 * 12) / 4 )
                                 .foregroundColor(.white)
-                                .background(Color.yellow)
-                                .cornerRadius(self.buttonWidth())
+                                .background(button.backgroundColor)
+                                .cornerRadius(self.buttonWidth(button: button))
+                            }
                         }
                     }
                 }
             }.padding(.bottom)
         }
     }
-    func buttonWidth() -> CGFloat {
+    func buttonWidth(button: CalcButtons) -> CGFloat {
+        if (button == .zero) {
+            return (UIScreen.main.bounds.width - 4 * 12) / 4 * 2
+        }
         //5 Gaps in 12 buttons
         return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
@@ -53,6 +117,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(GlobalEnvironment())
     }
 }
